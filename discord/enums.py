c0fbb8049193666b615f08a3db380191e48c8abf
DankBot4 <DankBot4@users.noreply.github.com>
2021-12-05 17:16:06 +0530

    added

--- discord/enums.py
+++ discord/enums.py
@@ -511,24 +511,23 @@ class AuditLogAction(Enum):
 
 
 class UserFlags(Enum):
-    staff = 1
-    partner = 2
-    hypesquad = 4
-    bug_hunter = 8
-    mfa_sms = 16
-    premium_promo_dismissed = 32
-    hypesquad_bravery = 64
-    hypesquad_brilliance = 128
-    hypesquad_balance = 256
-    early_supporter = 512
-    team_user = 1024
-    system = 4096
+    staff                      = 1
+    partner                    = 2
+    hypesquad                  = 4
+    bug_hunter                 = 8
+    mfa_sms                    = 16
+    premium_promo_dismissed    = 32
+    hypesquad_bravery          = 64
+    hypesquad_brilliance       = 128
+    hypesquad_balance          = 256
+    early_supporter            = 512
+    team_user                  = 1024
+    system                     = 4096
     has_unread_urgent_messages = 8192
-    bug_hunter_level_2 = 16384
-    verified_bot = 65536
-    verified_bot_developer = 131072
-    discord_certified_moderator = 262144
-
+    bug_hunter_level_2         = 16384
+    verified_bot               = 65536
+    verified_bot_developer     = 131072
+    spammer                    = 1048576
 
 class ActivityType(Enum):
     unknown = -1
