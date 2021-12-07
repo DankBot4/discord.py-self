b36df78fa89556c1e172dd9e73b35b7892f23f47
DankBot4 <95562568+DankBot4@users.noreply.github.com>
2021-12-07 15:35:17 +0530

    rollback

--- discord/http.py
+++ discord/http.py
@@ -55,7 +55,7 @@ async def json_or_text(response):
     return text
 
 class Route:
-    BASE = 'https://discord.com/api/v9'
+    BASE = 'https://discord.com/api/v7'
 
     def __init__(self, method, path, **parameters):
         self.path = path
